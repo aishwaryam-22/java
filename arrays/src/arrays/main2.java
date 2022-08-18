d99ce541a920d560cd4da402c3dad4e8ac21ad01
user <user@admin>
2022-08-19 01:09:02 +0530

    first commit

---
+++ arrays/src/arrays/main2.java
@@ -0,0 +1,14 @@
+package arrays;
+import java.util.*;
+public class main2 {
+
+	public static void main(String[] args) {
+		int [] arr = {12, 4, 5, 2, 5};
+		for (int i : arr){ {
+		System.out.print(i);
+
+		}
+
+	}
+
+}}
